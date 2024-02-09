10c74afdd05e31cfd2fd403dae109ac3f5681d1f
Sedad Sultanovic <sultansedad@gmail.com>
2024-02-09 09:38:30 +0100

    added API to link devicetypes with outlets

--- src/main/java/org/energy/abacus/logic/DeviceTypeService.java
+++ src/main/java/org/energy/abacus/logic/DeviceTypeService.java
@@ -1,6 +1,7 @@
 package org.energy.abacus.logic;
 
 import org.energy.abacus.entities.DeviceType;
+import org.energy.abacus.entities.Outlet;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -23,5 +24,16 @@ public class DeviceTypeService {
         return entityManager.createNamedQuery("findAllDeviceTypesById", DeviceType.class)
                 .setParameter("ids", deviceTypeIds)
                 .getResultList();
+    }
+
+    public int connectDeviceTypeToOutlet(int deviceTypeId, int outletId) {
+        DeviceType deviceType = entityManager.find(DeviceType.class, deviceTypeId);
+        if(deviceType == null){
+            throw new IllegalArgumentException("Device type with id " + deviceTypeId + " does not exist");
+        }
+        var outlet = entityManager.find(Outlet.class, outletId);
+        outlet.getDeviceTypes().add(deviceType);
+        deviceType.getOutlets().add(outlet);
+        return deviceType.getId();
     }
 }
--- src/main/java/org/energy/abacus/resource/DeviceTypeResource.java
+++ src/main/java/org/energy/abacus/resource/DeviceTypeResource.java
@@ -5,6 +5,7 @@ import org.energy.abacus.entities.DeviceType;
 import org.energy.abacus.logic.DeviceTypeService;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import java.util.Collection;
 
@@ -21,5 +22,11 @@ public class DeviceTypeResource {
     public Collection<DeviceType> getAll(){return service.getDeviceTypeList();}
 
     //Can we already assign a device type to a device?
+    @POST
+    @Authenticated
+    @Transactional
+    public int connectDeviceTypeToOutlet(@QueryParam("deviceTypeId") int deviceTypeId, @QueryParam("outletId") int outletId){
+        return service.connectDeviceTypeToOutlet(deviceTypeId, outletId);
+    }
 
 }
