45baadc3e6f30cf16057b729e2ae54e9231ccb24
Elias Gstallnig <e.gstallnig@gmail.com>
2024-02-29 22:40:18 +0100

    try again

--- src/main/java/org/energy/abacus/dtos/GetSumWattDto.java
+++ src/main/java/org/energy/abacus/dtos/GetSumWattDto.java
@@ -1,7 +1,16 @@
 package org.energy.abacus.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.Instant;
 
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetSumWattDto {
     private Instant timeStamp;
     private double wattPowerSum;
